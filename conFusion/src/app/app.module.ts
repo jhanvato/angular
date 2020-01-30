39c42685dd62d94fc78b38c28d28d33237f91b53
Jhon Valer <jhanvato1101@hotmail.com>
2020-01-30 16:06:34 -0300

    primer cambio

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
+	BrowserAnimationsModule,
+	MatListModule,
+    MatToolbarModule,
+	 MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
