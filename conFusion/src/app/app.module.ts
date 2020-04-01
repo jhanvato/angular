6bffeac636fe273ba13528ead1231007fc447790
Jhon Valer <jhanvato1101@hotmail.com>
2020-03-31 21:17:09 -0300

    Template Forms Part 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms'; 
+
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
@@ -23,6 +29,7 @@ import { PromotionService} from './services/promotion.service';
 import { LeaderService} from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +40,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -42,17 +50,24 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   	MatListModule,
     MatToolbarModule,
     MatGridListModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
-
+    MatDialogModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
   DishService,
   PromotionService,
   LeaderService
   
-	],
+  ],
+  entryComponents: [
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
