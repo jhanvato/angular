02014c123ec10298a95781833721945ccec4aaf5
Jhon Valer <jhanvato1101@hotmail.com>
2020-04-05 19:20:02 -0300

    Saving Changes to Server

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -19,6 +19,7 @@ export class DishdetailComponent implements OnInit {
   next: string;
   comment: Comment;
   dish: Dish;
+  dishcopy: Dish;
   errMess: string;
 
   @ViewChild('fform') commentFormDirective;
@@ -53,7 +54,7 @@ export class DishdetailComponent implements OnInit {
     this.createForm();
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); }, 
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); }, 
       errmess => this.errMess = <any>errmess);
   }
 
@@ -83,8 +84,13 @@ export class DishdetailComponent implements OnInit {
     console.log(this.CommentForm.value);
     this.comment = this.CommentForm.value;
     this.comment.date = new Date().toISOString();
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
     //this.dishservice.pushComment(this.CommentForm.value);
+    this.dishservice.putDish(this.dishcopy)
+      .subscribe(dish => {
+        this.dish = dish; this.dishcopy = dish;
+      },
+      errmess => { this.dish = null; this.dishcopy = null; this.errMess = <any>errmess; });
     this.commentFormDirective.resetForm();
     this.CommentForm.reset({
       author: '',
--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -3,7 +3,7 @@ import { Dish } from '../shared/dish';
 import { Observable,of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { map, catchError } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { baseURL } from '../shared/baseurl';
 import { ProcessHTTPMsgService } from './process-httpmsg.service';
 
@@ -27,7 +27,7 @@ export class DishService {
   }
 
   getFeaturedDish(): Observable<Dish> {
-    return this.http.get<Dish[]>(baseURL + 'dishees?featured=true').pipe(map(dishes => dishes[0]))
+    return this.http.get<Dish[]>(baseURL + 'dishes?featured=true').pipe(map(dishes => dishes[0]))
       .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
@@ -36,4 +36,14 @@ export class DishService {
       .pipe(catchError(error => error));
   }
 
+  putDish(dish: Dish): Observable<Dish> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.http.put<Dish>(baseURL + 'dishes/' + dish.id, dish, httpOptions)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+
+  }
 }
